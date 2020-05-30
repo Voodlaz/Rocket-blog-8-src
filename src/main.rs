fbc0ed0be4b920eaa275f0aab963a99f23430f56
Voodlaz <47495939+Voodlaz@users.noreply.github.com>
2020-05-30 17:35:47 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,39 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate diesel;
+#[macro_use] extern crate serde_derive;
+
 
 mod db_conn;
 mod schema;
 mod posts;
-use crate::posts::handlers;
+use crate::posts::{handlers, forms};
+use forms::Post;
 
 use rocket_contrib::templates::Template;
-use std::collections::HashMap;
+
 use rocket_contrib::serve::StaticFiles;
 
+use crate::schema::posts::dsl::*;
+use diesel::RunQueryDsl;
+use db_conn::DbConn;
+
+use tera::Context;
+
 #[get("/")]
-fn homepage() -> Template {
-    let context: HashMap<&str, &str> = HashMap::new();
+fn homepage(conn: DbConn) -> Template {
+    let mut context = Context::new();
+    let all_posts = posts.load::<Post>(&*conn).unwrap();
+
+    context.insert("posts", &all_posts);
     Template::render("homepage", &context)
 }
 
 fn main() {
     rocket::ignite()
+    .attach(DbConn::fairing())
     .attach(Template::fairing())
     .mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")))
-    .mount("/", routes![homepage, handlers::new_post, handlers::new_post_form])
+    .mount("/", routes![homepage, handlers::post_handler, handlers::new_post, handlers::new_post_form])
     .launch();
 }
